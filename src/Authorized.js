eb7c103df03eab81ba954b53b083b10f5df61a08
Arthur Song <arth3rs0ng@gmail.com>
2020-08-22 12:14:54 -0700

    Authorized component displays files titles from google drive

--- src/Authorized.js
+++ src/Authorized.js
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+import qs from 'qs';
+import axios from 'axios';
+// import { }
 
-const Authorized = () => {
+const Authorized = ({ }) => {
+    const [files, setFiles] = useState([]);
+    // console.log(match);
+
+    const history = useHistory();
+    // console.log(history);
+    // console.log(queryObject);
+    useEffect(() => {
+        const queryObject = qs.parse(history.location.hash);
+        if (queryObject['#state'] === 'oauth') {
+            // we are authorized..
+            axios.get('https://www.googleapis.com/drive/v2/files', {
+                headers: {
+                    authorization: `Bearer ${queryObject.access_token}`
+                }
+            })
+                .then(resp => {
+                    console.log(resp.data.items);
+                    setFiles(resp.data.items.map(i => i.title))
+                    // resp.data.items.forEach((i, index) => {
+                    //     setFiles(files => [...files, i.title])
+                    // })
+                });
+        } else {
+            // we aren't authorized.
+        }
+        return () => {
+            
+        }
+    }, [history])
+
+    console.log('files', files)
     return <div>
-           You are authorized. 
+        You are authorized.
+        {/* File titles */}
+        {files.length > 0 && files.map((f, index) => <h6 key={index}>
+            {f}
+        </h6>)}
     </div>
 }
 
