5842a1c9b5e01ebe9d8ba8ccde9043f932eec436
Arthur Song <arthursong14@gmail.com>
2020-08-28 15:51:00 -0700

    Positioning User Div

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -8,12 +8,18 @@ function SideBar() {
   const files = useSelector(state => state.files);
   // console.log('files', files);
   return <div className="sideBar">
-    <div className="sideBar__user"><span className="sideBar__email">arthursong14@gmail.com</span><i class="sideBar__carrot fa fa-caret-down" /></div>
+    {/* <div className="sideBar__user"><span className="sideBar__email">arthursong14@gmail.com</span><i class="sideBar__carrot fa fa-caret-down" /></div> */}
+    <div className="sideBar__user"><div className="sideBar__email">arthursong14@gmail.com</div><i class="sideBar__carrot fa fa-caret-down" /></div>
     <div className="sideBar__button">New Note</div>
     <div className="sideBar__notes">
-      
-      {Object.keys(files).length > 0 && Object.entries(files).map((f, index) => <div key={index} className="sideBar__listItem"><InsertDriveFileOutlinedIcon />&nbsp;<span className="sideBar__title">{f[1].name}</span></div>)}
-      {/* <div className="sideBar__group">
+    {Object.keys(files).length > 0 && Object.entries(files).map((f, index) => <div key={index} className="sideBar__listItem"><InsertDriveFileOutlinedIcon />&nbsp;<span className="sideBar__title">{f[1].name}</span></div>)}
+    </div>
+  </div>
+}
+
+export default SideBar
+
+   {/* <div className="sideBar__group">
         <div className="sideBar__listItem --folder"><FolderOutlinedIcon />&nbsp;<span className="sideBar__title">Programming</span></div>
         <div className="sideBar__listItem --file"><InsertDriveFileOutlinedIcon />&nbsp;<span className="sideBar__title">Pt II Distributed Data</span></div>
         <div className="sideBar__listItem --file"><InsertDriveFileOutlinedIcon />&nbsp;<span className="sideBar__title">Random Number, Positive Infinity, and BNS</span></div>
@@ -21,8 +27,3 @@ function SideBar() {
       <div className="sideBar__listItem"><InsertDriveFileOutlinedIcon />&nbsp;<span className="sideBar__title">Array from Range, Product of array except self</span></div>
       <div className="sideBar__listItem"><InsertDriveFileOutlinedIcon />&nbsp;<span className="sideBar__title">Ruby hashes are ordered. (LRU cache problem)</span></div>
       <div className="sideBar__listItem"><InsertDriveFileOutlinedIcon />&nbsp;<span className="sideBar__title">Longest Substring without Repeating Chars</span></div> */}
-    </div>
-  </div>
-}
-
-export default SideBar
