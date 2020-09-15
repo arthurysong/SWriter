347603de68597d07023183c02d2bb4f684b0df85
Arthur Song <arthursong14@gmail.com>
2020-09-15 13:51:22 -0700

    Styling for Login Component

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -3,17 +3,21 @@ import { oauth } from '../../utils/googleDrive'
 import { oauth2 } from '../../utils/medium';
 import axios from 'axios'
 import { API_URL } from '../../utils/URL';
+import './Login.scss';
 
 const Login = () => {
     // console.log('process.env', process.env);
 
-    return <div>
-        <h1>SWriter</h1>
-        <p>Free synced Journal using Google Drive</p>
-        {/* <button onClick={oauth}>Continue With Google</button>     */}
-        {/* Here I want to click and log in via Medium... */}
-        {/* <button onClick={oauth}>Continue with Google</button> */}
-        <button onClick={oauth2}>Continue with Medium</button>
+    return <div className="login">
+        <div className="login__background"></div>
+        <div className="login__container">
+            <h1 className="login__title">SWriter</h1>
+            <p className="login__description">A journaling app that posts to Medium. Organize and automate blogging.</p>
+            {/* <button onClick={oauth}>Continue With Google</button>     */}
+            {/* Here I want to click and log in via Medium... */}
+            {/* <button onClick={oauth}>Continue with Google</button> */}
+            <div className="login__button" onClick={oauth2}>Continue with Medium</div>
+        </div>
     </div>
 }
 
