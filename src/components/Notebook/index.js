464c4f20490b999dfdb72f12b45fa0f4e749c118
Arthur Song <arthursong14@gmail.com>
2020-09-15 15:27:33 -0700

    SideBar properly handles notebook and note clicks

--- src/components/Notebook/index.js
+++ src/components/Notebook/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Notebook.scss';
 import Note from '../Note';
 import { useDispatch, useSelector } from 'react-redux';
@@ -6,18 +6,29 @@ import { setActiveNotebook } from '../../actions/index';
 
 
 const Notebook = ({ notebook, notebookIndex }) => {
-    // const [toggled, setToggled] = useState(false);
-    const [hidden, setHidden] = useState(false);
+    const [toggled, setToggled] = useState(false);
+    // const [hidden, setHidden] = useState(false);
     const dispatch = useDispatch();
     const activeNotebook = useSelector(state => state.activeNotebook);
 
+    useEffect(() => {
+        if (activeNotebook === notebookIndex) setToggled(true);
+    }, [activeNotebook])
+
+    const clickHandler = e => {
+        e.stopPropagation(); // prevents the outer event from firing.
+        setToggled(toggled => !toggled)
+    }
+
+
     return <div onClick={() => dispatch(setActiveNotebook(notebookIndex))} className="notebook">
-        <div className={`notebook__listItem ${activeNotebook === notebookIndex ? 'notebook--active' : '' }`}>
-            <i className={`notebook__carrot fa ${activeNotebook === notebookIndex ? 'fa-caret-down' : 'fa-caret-right' }`} />
+        <div onClick={() => setToggled(true)} className={`notebook__listItem ${activeNotebook === notebookIndex ? 'notebook--active' : '' }`}>
+            <i onClick={clickHandler}
+                className={`notebook__carrot fa ${toggled ? 'fa-caret-down' : 'fa-caret-right' }`} />
             &nbsp;<i className="notebook__bookIcon fas fa-book" />
             &nbsp;<div className="notebook__title">{notebook.name}</div>
         </div>
-        {activeNotebook === notebookIndex && notebook.notes.map((note, noteIndex) => <Note key={noteIndex} note={note} noteIndex={noteIndex} notebookIndex={notebookIndex}/>)}
+        {toggled && notebook.notes.map((note, noteIndex) => <Note key={noteIndex} note={note} noteIndex={noteIndex} notebookIndex={notebookIndex}/>)}
     </div>
 }
 
