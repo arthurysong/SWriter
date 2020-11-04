30d6c7aa2bd5a1478fa4660ac38b5e312e332724
Arthur <arthursong14@gmail.com>
2020-11-03 22:11:56 -0800

    Changed SWriter to MWriter

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -6,7 +6,7 @@ const Login = () => {
     return <div className="login">
         <div className="login__background"></div>
         <div className="login__container">
-            <h1 className="login__title">SWriter</h1>
+            <h1 className="login__title">MWriter</h1>
             <p className="login__description">A journaling app that posts to Medium. Organize and automate blogging.</p>
             <div className="login__button" onClick={oauth2}>Continue with Medium</div>
         </div>
