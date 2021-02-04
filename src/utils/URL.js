ff72200a161ea1888ae7221dc749e69317cc3b62
arth3rs0ng@gmail.com <arthursong14@gmail.com>
2021-02-03 21:44:29 -0800

    Change redirect url for production

--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,3 +1,4 @@
 export const API_URL = process.env.NODE_ENV === "development" ? `http://localhost:5000` : `https://mwriter-api.herokuapp.com`;
 export const PROD_API_URL = `https://mwriter-api.herokuapp.com`;
-export const CLIENT_URL = process.env.NODE_ENV === "development" ? `http://127.0.0.1:3000` : `http://mwriter.herokuapp.com`
+// export const CLIENT_URL = process.env.NODE_ENV === "development" ? `http://127.0.0.1:3000` : `http://mwriter.herokuapp.com`
+export const CLIENT_URL = process.env.NODE_ENV === "development" ? `http://127.0.0.1:3000` : `https://mwriter.app`;
