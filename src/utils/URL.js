b72b2ec9bd6ece6252f07c4823df69168d609e22
arth3rs0ng@gmail.com <arthursong14@gmail.com>
2021-02-05 19:41:11 -0800

    Try different redirect uri

--- src/utils/URL.js
+++ src/utils/URL.js
@@ -2,6 +2,6 @@ export const API_URL = process.env.NODE_ENV === "development" ? `http://localhos
 // export const API_URL = `https://mwriter-api.herokuapp.com`;
 export const PROD_API_URL = `https://mwriter-api.herokuapp.com`;
 // export const CLIENT_URL = process.env.NODE_ENV === "development" ? `http://127.0.0.1:3000` : `http://mwriter.herokuapp.com`
-export const CLIENT_URL = process.env.NODE_ENV === "development" ? `http://127.0.0.1:3000` : `https://mwriter.app`;
+export const CLIENT_URL = process.env.NODE_ENV === "development" ? `http://127.0.0.1:3000` : `https://www.mwriter.app`;
 // export const CLIENT_URL = `http://www.mwriter.app`;
 // export const CLIENT_URL = `http://127.0.0.1:3000`;
