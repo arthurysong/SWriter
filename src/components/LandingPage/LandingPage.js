c5ca3d12eff5ddc60a9aea82f8bdfacf67f4ba77
arth3rs0ng@gmail.com <arthursong14@gmail.com>
2021-02-08 22:03:09 -0800

    React spring animation for header image

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -3,6 +3,7 @@ import styled, { keyframes } from 'styled-components';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 // import { useHistory } from 'react-router-dom';
 import { useHistory } from "react-router-dom";
+import { useSpring, animated} from 'react-spring';
 
 import logo from '../../assets/images/logo.png';
 import blog from '../../assets/images/blog.svg';
@@ -204,7 +205,40 @@ const DropInUnderline = styled.span`
   }
 `;
 
-export default function LandingPage() {
+// const GrowFromCenter = styled.div`
+//   animation: createBox .25s;
+
+//   @keyframes createBox {
+//     from {
+//       transform: scale(0);
+//     }
+
+//     to {
+//       transform: scale(1);
+//     }
+//   }
+// `;
+
+const AnimatedDiv = styled(animated.div)`
+  transform: scale(${props => props.x});
+`;
+
+const GrowFromCenter2 = ({ children }) => {
+  const animatedProps = useSpring({ 
+    config: {
+      mass: 3,
+      // duration: 250,
+    },
+    transform: "scale(1)", from: { transform: "scale(0)" }});
+
+  console.log("x", animatedProps.x);
+
+  return <animated.div style={animatedProps}>{children}</animated.div>
+}
+
+export default () => {
+// export default function LandingPage() {
+  // const animatedProps = useSpring({ transform: "scale(1)", from: { transform: "scale(1)" }})
 
   return (
     <StyledLandingPage>
@@ -234,7 +268,9 @@ export default function LandingPage() {
             <Link to="/login"><Button black style={{ marginBottom: "24px", }}><P as={Bold}>Get Started</P></Button></Link>
             <Link to="/login" style={{ textDecoration: 'underline' }}>Already have an account? Log in</Link>
           </SectionText>
-          <img src={blog} alt="someone writing a blog on their laptop" width={600}/>
+          <GrowFromCenter2 >
+            <img src={blog} alt="someone writing a blog on their laptop" width={600}/>
+          </GrowFromCenter2>
         </SectionContainer>
       </section>
 
@@ -319,10 +355,14 @@ export default function LandingPage() {
               <ColumnItem>Contact Us</ColumnItem>
             </Column>
             <div style={{ display: 'flex', marginLeft: 'auto', }}>
-              <SocialIcon src={reddit} alt="reddit"/>
+              <SocialIcon xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 11.779c0-1.459-1.192-2.645-2.657-2.645-.715 0-1.363.286-1.84.746-1.81-1.191-4.259-1.949-6.971-2.046l1.483-4.669 4.016.941-.006.058c0 1.193.975 2.163 2.174 2.163 1.198 0 2.172-.97 2.172-2.163s-.975-2.164-2.172-2.164c-.92 0-1.704.574-2.021 1.379l-4.329-1.015c-.189-.046-.381.063-.44.249l-1.654 5.207c-2.838.034-5.409.798-7.3 2.025-.474-.438-1.103-.712-1.799-.712-1.465 0-2.656 1.187-2.656 2.646 0 .97.533 1.811 1.317 2.271-.052.282-.086.567-.086.857 0 3.911 4.808 7.093 10.719 7.093s10.72-3.182 10.72-7.093c0-.274-.029-.544-.075-.81.832-.447 1.405-1.312 1.405-2.318zm-17.224 1.816c0-.868.71-1.575 1.582-1.575.872 0 1.581.707 1.581 1.575s-.709 1.574-1.581 1.574-1.582-.706-1.582-1.574zm9.061 4.669c-.797.793-2.048 1.179-3.824 1.179l-.013-.003-.013.003c-1.777 0-3.028-.386-3.824-1.179-.145-.144-.145-.379 0-.523.145-.145.381-.145.526 0 .65.647 1.729.961 3.298.961l.013.003.013-.003c1.569 0 2.648-.315 3.298-.962.145-.145.381-.144.526 0 .145.145.145.379 0 .524zm-.189-3.095c-.872 0-1.581-.706-1.581-1.574 0-.868.709-1.575 1.581-1.575s1.581.707 1.581 1.575-.709 1.574-1.581 1.574z"/></SocialIcon>
+              <SocialIcon xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z"/></SocialIcon>
+              <SocialIcon xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></SocialIcon>
+              <SocialIcon width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M2.846 6.887c.03-.295-.083-.586-.303-.784l-2.24-2.7v-.403h6.958l5.378 11.795 4.728-11.795h6.633v.403l-1.916 1.837c-.165.126-.247.333-.213.538v13.498c-.034.204.048.411.213.537l1.871 1.837v.403h-9.412v-.403l1.939-1.882c.19-.19.19-.246.19-.537v-10.91l-5.389 13.688h-.728l-6.275-13.688v9.174c-.052.385.076.774.347 1.052l2.521 3.058v.404h-7.148v-.404l2.521-3.058c.27-.279.39-.67.325-1.052v-10.608z"/></SocialIcon>
+              {/* <SocialIcon src={reddit} alt="reddit"/>
               <SocialIcon src={linkedin} alt="linkedin"/>
               <SocialIcon src={twitter} alt="twitter"/>
-              <SocialIcon src={medium} alt="medium"/>
+              <SocialIcon src={medium} alt="medium"/> */}
             </div>
           </div>
           <SmallP style={{ opacity: 0.6 }}>© 2021 MWriter. All rights reserved.</SmallP>
@@ -335,14 +375,17 @@ export default function LandingPage() {
   )
 }
 
-const SocialIcon = styled.img`
+// export default LandingPage;
+
+const SocialIcon = styled.svg`
   margin: 0 10px;
-  width: 20px;
-  height: 20px;
+  // width: 20px;
+  // height: 20px;
 
   &:hover {
     cursor: pointer;
-    color: #3aa82d;
+    // color: #3aa82d;
+    fill: #3aa82d;
   }
 `;
 
